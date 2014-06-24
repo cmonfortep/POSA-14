8314bdfea0d663d178bdd8f04c1379c11367607e
McCulebrah <cristian.monforte@gmail.com>
2014-06-24 18:09:00 +0200

    week 6 done.

--- assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
+++ assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
@@ -2,6 +2,7 @@ package edu.vuum.mocca;
 
 import java.lang.ref.WeakReference;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
@@ -78,6 +79,7 @@ public class DownloadActivity extends DownloadBase {
                 // bitmap that's been downloaded and returned to
                 // the DownloadActivity as a pathname who's Bundle
             	// key is defined by DownloadUtils.PATHNAME_KEY
+				activity.displayBitmap(msg.getData().getString(DownloadUtils.PATHNAME_KEY));
             }
     	}
     }
@@ -101,13 +103,15 @@ public class DownloadActivity extends DownloadBase {
      */
     public void runService(View view) {
     	String which = "";
-
+    	
+    	Intent intentService = null;
+    	
     	switch (view.getId()) {
         case R.id.intent_service_button:
             // TODO - You fill in here to start the
             // DownloadIntentService with the appropriate Intent
             // returned from the makeIntent() factory method.
-
+        	intentService = DownloadIntentService.makeIntent(this, handler, super.getUrlString());
             which = "Starting IntentService";
             break;
         
@@ -115,11 +119,15 @@ public class DownloadActivity extends DownloadBase {
             // TODO - You fill in here to start the
             // ThreadPoolDownloadService with the appropriate Intent
             // returned from the makeIntent() factory method.
-
+        	intentService = ThreadPoolDownloadService.makeIntent(this, handler, super.getUrlString());
             which = "Starting ThreadPoolDownloadService";
             break;
         
         }
+    	
+    	if(intentService!=null){
+    		startService(intentService);
+    	}
 
     	// Display a short pop-up notification telling the user which
     	// service was started.
